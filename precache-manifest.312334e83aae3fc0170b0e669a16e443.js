701a206f140afd8d96dfdf459638bd8f2f9efab9
mentab <cleabr@oulook.fr>
2018-10-21 19:48:31 +0200

    Updates

---
+++ precache-manifest.312334e83aae3fc0170b0e669a16e443.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testdeploy/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "af77c1c4c87271addcd3",
+    "url": "/testdeploy/static/js/runtime~main.af77c1c4.js"
+  },
+  {
+    "revision": "cc8b2f81db5ec19c21e8",
+    "url": "/testdeploy/static/js/main.cc8b2f81.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/testdeploy/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "cc8b2f81db5ec19c21e8",
+    "url": "/testdeploy/static/css/main.5c48314a.chunk.css"
+  },
+  {
+    "revision": "fb5a0113fcbf20045d2b7e1c0032bcbf",
+    "url": "/testdeploy/index.html"
+  }
+];
